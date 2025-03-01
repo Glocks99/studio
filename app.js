9f73f0883c019e8bc47a04e291f11774a4d61e91
James <jameslarbie30@gmail.com>
2025-03-01 15:47:00 +0000

    done

--- app.js
+++ app.js
@@ -12,4 +12,51 @@ window.addEventListener("DOMContentLoaded", () => {
             menu.innerHTML = `<i class="fas fa-bars"></i>`
         }
     })
+
+    gsap.registerPlugin(ScrollTrigger)
+    
+
+    const tlIntro = gsap.timeline({defaults: {ease: "power3.out", duration: .75}})
+
+    tlIntro.fromTo(".logo", {y: '100%', opacity: 0}, {y: 0, opacity:1, duration: 1, delay: .5
+    })
+    tlIntro.fromTo(".hero-info img", {y: '100%', opacity: 0}, {y: 0, opacity:1, duration: 1}, '<50%')
+    tlIntro.fromTo(".hero-info h1", {y: '100%', opacity: 0}, {y: 0, opacity:1, duration: 1},'<50%')
+    tlIntro.fromTo(".hero-info p", {y: '100%', opacity: 0}, {y: 0, opacity:1, duration: 1})
+    tlIntro.fromTo(".hero-info button", {y: '100%', opacity: 0}, {y: 0, opacity:1, duration: 1},'<50%')
+    tlIntro.to('.hero', {scrollTrigger: {
+        trigger: '.hero',
+        start: "0",
+        pin: true,
+        pinSpacing: false,
+        end: "100%",
+        scrub: 1
+    }})
+
+
+    const tlcomment = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".cmt",
+            start: "0%",
+            pin: true,
+            pinSpacing: false,
+            end: "100%",
+            scrub: 1,
+        }
+    })
+
+    const tlGallery = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".gal-1",
+            start: "-30%",
+            pin: true,
+            pinSpacing: false,
+            end: "100%",
+            scrub: 1,
+        }
+    })
+
+    tlGallery.to(".gal-1", {x: "-100%"})
+    tlGallery.to(".gal-2", {x: "100%"}, "<")
+    tlGallery.to(".gal-3", {x: "-100%"}, "<")
 })
